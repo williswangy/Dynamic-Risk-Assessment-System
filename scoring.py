115b0bb1667f67a51ad9f1bc3d76a1a6beb3a15e
Yuan Wang <yxw017@cs.bham.ac.uk>
2023-06-16 20:27:57 +0100

    added the scoring function

--- scoring.py
+++ scoring.py
@@ -1,25 +1,64 @@
-from flask import Flask, session, jsonify, request
-import pandas as pd
-import numpy as np
-import pickle
 import os
-from sklearn import metrics
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
 import json
+import pickle
+import pandas as pd
+import logging
+from sklearn import metrics
+from training import segregate_dataset,filter_features
+import sys
 
-
+# Set up logging
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
 #################Load config.json and get path variables
-with open('config.json','r') as f:
-    config = json.load(f) 
+with open('config.json', 'r') as f:
+    config = json.load(f)
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
-test_data_path = os.path.join(config['test_data_path']) 
+dataset_csv_path = os.path.join(config['output_folder_path'])
+test_data_path = os.path.join(config['test_data_path'])
+model_path = os.path.join(config['output_model_path'])
 
+# import test dataset
+filepath = os.path.join(test_data_path, 'testdata.csv')
+testdata = pd.read_csv(filepath)
+
+def load_model(model_path: str):
+    """Loads the trained model from disk"""
+    try:
+        with open(model_path, 'rb') as f:
+            model = pickle.load(f)
+        logging.info("Model loaded successfully")
+        return model
+    except Exception as e:
+        logging.error(f"Error occurred while loading model: {e}")
+        raise
 
 #################Function for model scoring
 def score_model():
-    #this function should take a trained model, load test data, and calculate an F1 score for the model relative to the test data
-    #it should write the result to the latestscore.txt file
+    # this function should take a trained model, load test data, and calculate an F1 score for the model relative to the test data
+    # it should write the result to the latestscore.txt file
 
+    # load trained model
+    modelpath = os.path.join(model_path, 'trainedmodel.pkl')
+    model = load_model(modelpath)
+
+    # segregate test dataset
+    X, y = segregate_dataset(filter_features(testdata))
+
+    # evaluate model on test set
+    logging.info("Evaluating model on test set...")
+    yhat = model.predict(X)
+    score = metrics.f1_score(y, yhat)
+    logging.info(f"Computed F1 score: {score}")
+
+    # save as latest score
+    scorespath = os.path.join(model_path, 'latestscore.txt')
+    with open(scorespath, 'w') as f:
+        f.write(str(score))
+    logging.info(f"F1 score computed and saved to {scorespath}")
+
+    return score
+
+
+if __name__ == '__main__':
+    score_model()
--- training.py
+++ training.py
@@ -1,32 +1,74 @@
-from flask import Flask, session, jsonify, request
-import pandas as pd
-import numpy as np
-import pickle
 import os
-from sklearn import metrics
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
 import json
+import pickle
+import logging
+import sys
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
 
 ###################Load config.json and get path variables
-with open('config.json','r') as f:
-    config = json.load(f) 
+with open('config.json', 'r') as f:
+    config = json.load(f)
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
-model_path = os.path.join(config['output_model_path']) 
+dataset_csv_path = config['output_folder_path']
+model_path = config['output_model_path']
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+# Define the features to keep
 
 
-#################Function for training the model
-def train_model():
-    
-    #use this logistic regression for training
-    LogisticRegression(C=1.0, class_weight=None, dual=False, fit_intercept=True,
-                    intercept_scaling=1, l1_ratio=None, max_iter=100,
-                    multi_class='warn', n_jobs=None, penalty='l2',
-                    random_state=0, solver='liblinear', tol=0.0001, verbose=0,
-                    warm_start=False)
-    
-    #fit the logistic regression to your data
-    
-    #write the trained model to your workspace in a file called trainedmodel.pkl
 
+def load_dataset(filepath):
+    """Load dataset from csv file"""
+    logging.info(f"Loading dataset from {filepath}")
+    return pd.read_csv(filepath)
+
+
+def filter_features(dataset):
+    """Filter the dataset for necessary features"""
+    FEATURES = ['lastmonth_activity', 'lastyear_activity', 'number_of_employees', 'exited']
+    logging.info(f"Filtering features {FEATURES}")
+    return dataset[FEATURES]
+
+
+def segregate_dataset(dataset):
+    """Segregate the dataset into feature dataframe and target series"""
+    logging.info(f"Segregating dataset into features and target...")
+    X = dataset.drop(columns='exited')
+    y = dataset['exited']
+    return X, y
+
+
+def train_model(X, y, model_config):
+    """Train the model using the training data"""
+    logging.info(f"Training model with configuration: {model_config}")
+    model = LogisticRegression(**model_config)
+    model.fit(X, y)
+    return model
+
+
+def save_model(model, filepath):
+    """Save the trained model to disk"""
+    logging.info(f"Saving model to {filepath}")
+    os.makedirs(os.path.dirname(filepath), exist_ok=True)
+    with open(filepath, 'wb') as f:
+        pickle.dump(model, f)
+
+
+def main():
+    logging.info(f"Starting model training...")
+    dataset = load_dataset(os.path.join(dataset_csv_path, "finaldata.csv"))
+    dataset = filter_features(dataset)
+    X, y = segregate_dataset(dataset)
+
+    # Get the model config from `config.json`
+    model_config = config['model_config']
+    model = train_model(X, y, model_config)
+
+    save_model(model, os.path.join(model_path, 'trainedmodel.pkl'))
+
+    logging.info(f"Model training completed.")
+
+
+if __name__ == '__main__':
+    main()
